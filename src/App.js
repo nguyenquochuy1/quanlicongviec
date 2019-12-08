e9c07602aeef5c994a729cc2c79d28f42d68a19d
nquochuy <nquochuy1995@gmail.com>
2019-12-08 21:58:09 +0900

    Fix compoment

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
         </div>
         <div className="row mt-15">
           <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <TaskList/>
+            <TaskForm/>
           </div>
           <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
             <button type="button" className="btn btn-primary">
@@ -29,7 +29,7 @@ class App extends React.Component {
             
             <div className="row mt-15">
               <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <TaskForm/>
+                <TaskList/>
               </div>
             </div>
           </div>
